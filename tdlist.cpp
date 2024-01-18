27dc6d28fb97122c87fd1fa08dc9507fcc62493a
Karim Elkhanoufi <126080269+karimelkh@users.noreply.github.com>
2024-01-18 21:39:25 +0100

    fix menu
    i've forgot the  in case 4 and 5 😅

--- tdlist.cpp
+++ tdlist.cpp
@@ -14,8 +14,8 @@ int main() {
       case 1: add(todo_list); break;
       case 2: del(todo_list); break;
       case 3: todo_list.display_tasks(); break;
-      case 4: stats(todo_list);
-      case 5: edit(todo_list);
+      case 4: stats(todo_list); break;
+      case 5: edit(todo_list); break;
       case 0: break;
       default: std::cout << "Invalid choice." << std::endl;
     }
